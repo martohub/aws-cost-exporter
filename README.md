c0273309f48179c96bbaf98f941221fee12a69dd
Martin <martinvyotov@gmail.com>
2021-03-29 09:00:21 +0300

    implemented additional metric and api calls

--- README.md
+++ README.md
@@ -1,17 +1,78 @@
 # Prometheus AWS Cost Exporter
 
+
+1. implemented second API call for fetching both blended/unblended metric  
+2. implemented API call to STS to fetch account number and have it in the metrics  
+
+to do: hopefully, implement assume role functionality with Alex Shamara & Lil Polly
+
+#login with aws configure, pip3 install boto3 prometheus_client  
+git clone https://github.com/martohub/aws-cost-exporter && cd aws-cost-exporter  
+export TAG_PROJECT='environment' && ./aws-cost-exporter.py 
+ 
+http://localhost:9050  
+
+```
+# HELP aws_monthly_blended_cost in USD by tag
+# TYPE aws_monthly_blended_cost gauge
+aws_monthly_blended_cost{account_number="111111111111",environment="not_defined_by_the_tag"} 2.75
+aws_monthly_blended_cost{account_number="111111111111",environment="dev"} 8.35
+aws_monthly_blended_cost{account_number="111111111111",environment="qa"} 3.17
+aws_monthly_blended_cost{account_number="111111111111",environment="test"} 5.48
+# HELP aws_monthly_unblended_cost in USD by tag
+# TYPE aws_monthly_unblended_cost gauge
+aws_monthly_unblended_cost{account_number="111111111111",environment="not_defined_by_the_tag"} 2.75
+aws_monthly_unblended_cost{account_number="111111111111",environment="dev"} 8.35
+aws_monthly_unblended_cost{account_number="111111111111",environment="qa"} 3.17
+aws_monthly_unblended_cost{account_number="111111111111",environment="test"} 5.48
+```
+
+# API string debugging
+
+python shell:
+
+```
+import time
+import os
+import re
+import boto3
+import datetime
+now = datetime.datetime.utcnow()
+end = datetime.datetime(year=now.year, month=now.month, day=now.day)
+start = end - datetime.timedelta(days=30)
+start = start.strftime('%Y-%m-%d')
+end = end.strftime('%Y-%m-%d')
+tagProject = 'application_id'
+client = boto3.client('ce')
+client.get_cost_and_usage(
+	TimePeriod={
+		'Start': start,
+		'End':  end
+	},
+	Granularity='MONTHLY',
+	Metrics=['UnblendedCost'],
+	GroupBy=[
+		{
+			'Type': 'TAG',
+			'Key': tagProject 
+		},
+	]
+)
+```
+
 # Description
 
-This export will query AWS Cost Explorer API search by a specific tag. This is usefull for know costs of a specific project. All you need to do is run the docker with the env variables. The script will discovery and return the amount for each value founded on specified tag.
+This exporter will query AWS Cost Explorer API search by a specific tag. This is useful for know the cost of a specific project. All you need to do is run it with the env variables. The script will discover and return the amount for each value founded on a specified tag.
+
 
 # Authentication
 
-The script use boto, so, if you already have aws/credentials file, the script will work's fine. You can use env variables also:
+The script uses boto, so, if you already have aws/credentials file, the script will work fine. You can use env variables also:
 
 |Variable|Description|Default value|
 |--------|-----------|-------------|
 |PORT|Port for exporter listener| 9150|
-|TAG_PROJECT|Tag to search|Scost|
+|TAG_PROJECT|Tag to search|environment|
 |AWS_DEFAULT_REGION| Your AWS REGION|-|
 |AWS_ACCESS_KEY_ID|Your AWS Key|-|
 |AWS_SECRET_ACCESS_KEY|Your AWS SECRET KEY|-|
@@ -28,8 +89,8 @@ docker run --restart always -d -p 9150:9150 -e TAG_PROJECT="YOUR_TAG_PROJECT" -e
 
 ```
 - job_name: aws-cost-exporter
-  scrape_interval: 1800s
-  scrape_timeout: 60s
+  scrape_interval: 3600s
+  scrape_timeout: 300s
   static_configs:
   - targets:
     - localhost:9150
@@ -55,4 +116,4 @@ docker run --restart always -d -p 9150:9150 -e TAG_PROJECT="YOUR_TAG_PROJECT" -e
 
 ######
 
-PR, comments and enhancements are always welcome
+PR, comments, and enhancements are always welcome
--- aws-cost-exporter.py
+++ aws-cost-exporter.py
@@ -6,14 +6,17 @@ import os
 import re
 import boto3
 import datetime
-
 #Try get the TAG_PROJECT env variable. If not defined, we will use the Scost
-tagProject = os.getenv('TAG_PROJECT','Scost')
+tagProject = os.getenv('TAG_PROJECT','environment')
+#tagProject1 = os.getenv('TAG_PROJECT','application_id')
+
 
 #Try get the PORT env variable. If not defined, we will use the 9150 
 port = os.getenv('PORT',9150)
 
 def getCosts():
+
+
 	#Create a boto3 connection with cost explorer
 	client = boto3.client('ce')
 
@@ -24,7 +27,7 @@ def getCosts():
 	end = datetime.datetime(year=now.year, month=now.month, day=now.day)
 
 	# Subtract a day to define the start of the range 
-	start = end - datetime.timedelta(days=1)
+	start = end - datetime.timedelta(days=30)
 
 	# Convert them to strings
 	start = start.strftime('%Y-%m-%d')
@@ -39,7 +42,7 @@ def getCosts():
 			'Start': start,
 			'End':  end
 		},
-		Granularity='DAILY',
+		Granularity='MONTHLY',
 		Metrics=['BlendedCost'],
 		GroupBy=[
 			{
@@ -48,9 +51,27 @@ def getCosts():
 			},
 		]
 	)
-
 	#Create an empty dictionary
 	projectValues = {}
+
+
+#nov api kol i biblioteka za unblended
+	response1 = client.get_cost_and_usage(
+		TimePeriod={
+			'Start': start,
+			'End':  end
+		},
+		Granularity='MONTHLY',
+		Metrics=['UnblendedCost'],
+		GroupBy=[
+			{
+				'Type': 'TAG',
+				'Key': tagProject
+			},
+		]
+	)
+	projectValues1 = {}
+
 
 	#Run the response and make a dictionary with tag name and tag value
 	for project in response["ResultsByTime"][0]["Groups"]:
@@ -61,7 +82,7 @@ def getCosts():
 
 		#If name is none, let's defined it as Other
 		if name is None or name == "":
-			name = "Other"
+			name = "not_defined_by_the_tag"
 
 		#Get the value
 		amount = project['Metrics']['BlendedCost']['Amount']
@@ -70,9 +91,24 @@ def getCosts():
 
 		#Append the values in the directionary
 		projectValues[name] = float(amount)
-
 	#Return the dictionary with all those values
 	return projectValues
+
+
+
+#zavurti go pak, hvani s regex i vurni valueto v novata biblioteka
+
+	for project1 in response1["ResultsByTime"][0]["Groups"]:
+		namestring1 = project1['Keys'][0]
+		name1 = re.search("\$(.*)", namestring1).group(1)
+		if name1 is None or name1 == "":
+			name1 = "not_defined_by_the_tag1"
+		amount1 = project1['Metrics']['BlendedCost']['Amount']
+		amount1 = "{0:.2f}".format(float(amount1))
+		projectValues1[name] = float(amount1)
+	return projectValues1
+
+
 
 
 #Start classe collector
@@ -82,21 +118,26 @@ class costExporter(object):
 
 		#Expose the metric
 		#Create header
-		metric = Metric('aws_project_cost','Total amount of costs for project','gauge')
-
+		metric = Metric('aws_monthly_blended_cost','in USD by tag','gauge')
 		#Run the retuned dictionary and expose the metrics
-		for project,cost in getCosts().items():
-			metric.add_sample('aws_project_cost',value=cost,labels={'project':project})
+		
+		#nov API call kum sts-a za da vzema account nomera
+		account_number = boto3.client('sts').get_caller_identity().get('Account')
 
+		for project,cost in getCosts().items():
+			metric.add_sample('aws_monthly_blended_cost',value=cost,labels={'account_number':account_number,tagProject:project})
 		#/Expose the metric
 		yield metric
+
+		###zapishi i novata biblioteka
+		metric1 = Metric('aws_monthly_unblended_cost','in USD by tag','gauge')
+		for project1,cost1 in getCosts().items():
+			metric1.add_sample('aws_monthly_unblended_cost',value=cost1,labels={'account_number':account_number,tagProject:project1})
+		yield metric1
 
 if __name__ == '__main__':
 
 	start_http_server(port)
-        
 	metrics = costExporter()
-	
 	REGISTRY.register(metrics)
-
 	while True: time.sleep(1) 
